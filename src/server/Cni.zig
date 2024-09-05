47fd953318f04b66c9a4671fd76730b34a6c94a4
Songmin Li <lisongmin@protonmail.com>
2024-09-05 10:57:34 +0800

    fix(plugin): The respose of create should be the same as original request

--- src/server/Cni.zig
+++ src/server/Cni.zig
@@ -62,7 +62,7 @@ pub fn deinit(self: Cni) void {
 pub fn create(self: *Cni, tentative_allocator: Allocator, request: plugin.Request, responser: *Responser) !void {
     _ = self;
     _ = tentative_allocator;
-    responser.write(request);
+    responser.write(request.request.network);
 }
 
 const CniCommand = enum {
